6ee9202b1dc48cb6e213f1b5621fa2d3c5c61c77
Parimala-1206 <beedupalliparimala02@gmail.com>
2024-12-09 11:49:33 +0530

    fir com

---
+++ vow.java
@@ -0,0 +1,31 @@
+class vowel
+{
+	public static void main(String[] args)
+{
+
+	char ch='u';
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+	switch(ch)
+	{
+	case 'a':
+		System.out.println("apple");break;
+	case 'e':
+		System.out.println("elephant");break;
+	case 'i':
+		System.out.println("iron");break;
+	case 'o':
+		System.out.println("oninon");break;
+	default:
+		System.out.println("umbrealla");break;
+	}
+	}
+	else
+		System.out.println("not a vowel");
+
+	
+
+		
+}
+}
+
